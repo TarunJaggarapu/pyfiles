04bb84b01b8c4b63d11406c70b88b59f324a19b5
TarunJaggarapu <tarun18teja@gmail.com>
2023-02-18 00:22:13 +0530

    python files

---
+++ app.py
@@ -0,0 +1,10 @@
+dict = {"tarun":"teja",
+        "Sasuke" : "uchiha",
+        "uzumaki" : "naruto",
+         "Sasi": "kunar",
+         "hyuga": {"hinata":"neji"},
+         "rank": [12,332,43]}
+print(dict.items())
+print(dict["hyuga"]["hinata"])
+print(dict.keys())
+print(dict.values())
